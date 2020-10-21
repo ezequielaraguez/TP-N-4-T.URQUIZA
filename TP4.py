67d92b5d994ef5364828d266b6594eda260ebe55
Ezequiel <araguez.l.e@gmail.com>
2020-10-21 18:31:39 -0300

    indica si el numero ingresado es menor o mayor al correcto, si es correcto ganaste

--- TP4.py
+++ TP4.py
@@ -24,3 +24,35 @@ def generanumero (menor, mayor):
     return randint(menor, mayor)
 
 NumeroCorrecto = randint(menor, mayor)
+
+#Segun la opcion elegimos
+if OpcionIngresada == 1:
+    #Ingresamos el numero que nosotros elegimos.
+    NumeroElegido = int(input("Ingresa un numero entre 1 y 100: "))
+
+    #Una ayuda si el numero es mayor o menor al elegido
+    if NumeroElegido > NumeroCorrecto:
+        print("Es un numero menor al elegido.")
+    elif NumeroElegido == NumeroCorrecto:
+        print("Correcto Felicitaciones el numero es : ", NumeroCorrecto)
+    else:
+        print("El numero es mayor al elegido.")
+
+    #Entra en un bucle que no va a salir hasta que sea el numero correcto
+    while NumeroElegido != NumeroCorrecto:
+        print("Incorrecto intentalo de nuevo.")
+        #Pedimos un numero si no es correcto
+        NumeroElegido = int(input("Ingresa un numero entre 1 y 100: "))
+
+        #Una ayuda si el numero es mayor o menor al elegido
+        if NumeroElegido > NumeroCorrecto:
+            print("Es un numero menor al elegido.")
+        elif NumeroElegido == NumeroCorrecto:
+            print("")
+        else:
+            print("El numero es mayor al elegido.")
+
+    print("Correcto Felicitaciones el numero es : ", NumeroCorrecto)
+
+else:
+    print("Gracias por jugar. Chau")
